c88bcd62777120487f83deb1a5468c4322a0a52b
aafsari <amirafsari99@yahoo.com>
2023-01-30 19:50:42 +0330

    add users list

--- src/router.js
+++ src/router.js
@@ -2,8 +2,16 @@ import {Router} from 'express'
 
 const router = Router()
 
-router.get('/profile', (req, res, next) => {
-    res.json({username:'user1', email:'user1@gmail.com'})
+const users = [
+    {id:1, username:'user1', email:'user1@gmail.com'},
+    {id:2, username:'user2', email:'user2@gmail.com'}]
+
+router.get('/profile/:id', (req, res, next) => {
+    const {id} = req.params
+    const user = users.find(u=>u.id===Number(id))
+    console.log(user);
+    res.json(user)
+
 })
 
 export default router
